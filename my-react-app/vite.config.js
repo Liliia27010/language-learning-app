0b6e3fe4ac35f792633749bad33ca7e489f852a9
Liliia <pla.soryvei@gmail.com>
2026-03-05 12:40:21 +0200

    fix variable

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: import.meta.env.BASE_URL,
+        target: import.meta.env.VITE_BASE_URL,
         changeOrigin: true,
         secure: false,
       },
