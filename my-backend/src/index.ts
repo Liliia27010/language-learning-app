99d162c5340c42ed6ed0faf6fc2ef652de3d0b71
Liliia <pla.soryvei@gmail.com>
2026-03-05 12:25:28 +0200

    add env variables

--- my-backend/src/index.ts
+++ my-backend/src/index.ts
@@ -15,7 +15,7 @@ const run = () => {
 
     app.use(
       cors({
-        origin: "http://localhost:5173", // Replace with your frontend's origin
+        origin: process.env.FRONTEND_URL || "http://localhost:5173", // Replace with your frontend's origin
         methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
         credentials: true, // Allow credentials (cookies, authorization headers, etc.)
         allowedHeaders: ["Content-Type", "Authorization"],
